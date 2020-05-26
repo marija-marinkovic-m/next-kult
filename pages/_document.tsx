bb0ff5d7d0f42855b694320ef197a7bc26bc3e47
marija-marinkovicm <marija@tourradar.com>
2020-05-26 14:27:39 +0200

    setup the blog

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="sh">
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/noto-sans-v9-latin-regular.woff2"
+            as="font"
+            crossOrigin=""
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
